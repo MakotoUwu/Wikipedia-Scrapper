70d08a2f7e75e83e7e4886501d08100e5a81f6a4
Aleksandr <tsepuh.mamoru@gmail.com>
2023-06-23 19:44:50 +0200

    added coments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Import the leaders_scraper module from the Wikipedia-Scrapper repository
 import leaders_scraper
 
+# Call the check_scraping function to verify if the scraping process is successful
 leaders_scraper.check_scraping()
