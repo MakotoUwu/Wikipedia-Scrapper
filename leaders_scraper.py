e05320e9a4c77ed157c4d465dffc92d1ed7ce0b7
Aleksandr <tsepuh.mamoru@gmail.com>
2023-06-23 10:48:21 +0200

    added main.py

--- leaders_scraper.py
+++ leaders_scraper.py
@@ -6,7 +6,7 @@ import time
 
 def greet_user():
     """
-    Displays a greeting message, and explains the purpose of this script to the user.
+    Displays a greeting messages to the user.
     """
     print("\n===============================")
     print("Hello! Welcome to our scraper!")
@@ -19,86 +19,97 @@ def greet_user():
       /   \\ 
     """)
     
-    print("Scattering our cobwebs...") 
+    print("Scattering our cobwebs...\n") 
     print("Please wait while we are scraping the data...\n")
 
 
 def save(leaders_per_country):
     """
-    Saves the leaders' data to a json file, replacing None values with 'Unknown information'
+    Function to save the leaders' data to a JSON file.
+    It replaces all None values with a placeholder string 'Unknown information'.
     """
-    # Replace None values with a default string
+    # Loop over the countries in the leaders_per_country dictionary
     for country in leaders_per_country:
+        # Loop over the leaders in the current country
         for leader in leaders_per_country[country]:
+            # Loop over the attributes of the current leader
             for key, value in leader.items():
+                # If an attribute's value is None, replace it with a default string
                 if value is None:
                     leader[key] = 'Unknown information'
 
-    # Save leaders_per_country to leaders.json
+    # Save the leaders_per_country dictionary to a JSON file
     with open('leaders.json', 'w', encoding='utf-8') as f:
+        # dump() function writes the Python dictionary to the JSON file
+        # indent=4 makes the file human-readable with 4 spaces of indentation
+        # sort_keys=True sorts the keys alphabetically
+        # ensure_ascii=False ensures that non-ASCII characters are saved correctly
         json.dump(leaders_per_country, f, indent=4, sort_keys=True, ensure_ascii=False)
+
 
 def get_leaders():
     """
-    Retrieves leaders' data from the given URL and stores them in a dictionary.
-    Also handles session cookies and HTTP errors.
+    Function to retrieve leaders' data from a predefined website.
+    It manages session cookies, HTTP errors, and stores the resulting data into a dictionary.
     """
-    # Define the URLs
+    # Set the root, countries, leaders, and cookie URLs
     root_url = "https://country-leaders.onrender.com"
     countries_url = root_url + "/countries"
     leaders_url = root_url + "/leaders"
     cookie_url = root_url + "/cookie"
 
-    # Create a session
+    # Create a web session using requests.Session()
     session = requests.Session()
 
-    # Get the initial cookies
+    # Retrieve the initial cookies from the cookie URL
     cookies = session.get(cookie_url).cookies
 
-    # Get the countries
+    # Get the list of countries from the countries URL
     countries_response = session.get(countries_url, cookies=cookies)
 
-    # Check if there is a cookie error
+    # If an authorization error (401) occurs, refresh the cookies and try again
     if countries_response.status_code == 401:
-        # Get new cookies
         cookies = session.get(cookie_url).cookies
         countries_response = session.get(countries_url, cookies=cookies)
 
-    # Check if there is an exception or another error
+    # If there's any other kind of error, raise an exception
     countries_response.raise_for_status()
 
+    # Convert the response to JSON format
     countries = countries_response.json()
 
-    # Create an empty dictionary
+    # Initialize an empty dictionary to store the leaders
     leaders_per_country = {}
 
-    start_time = time.time()  # Start the timer
+    start_time = time.time()  # Start the timer for performance tracking
 
-    # Loop through the countries
+    # Loop through the list of countries
     for country in countries:
         params = {"country": country}
         leaders_response = None
 
         try:
+            # Try to get the leaders for the current country
             leaders_response = session.get(leaders_url, cookies=cookies, params=params)
 
-            # Retry with new cookies if a "403 Forbidden" error occurs
+            # If a "403 Forbidden" error occurs, refresh the cookies and try again
             while leaders_response.status_code == 403:
-                # Get new cookies
                 cookies = session.get(cookie_url).cookies
                 leaders_response = session.get(leaders_url, cookies=cookies, params=params)
 
-            # Check if there is an exception or another error
+            # If there's any other kind of error, raise an exception
             leaders_response.raise_for_status()
 
         except requests.exceptions.HTTPError as e:
+            # If an HTTP error occurs, print the error and continue with the next country
             print("An error occurred:", e)
 
+        # If the response is successful, process the leaders
         if leaders_response is not None:
             leaders = leaders_response.json()
             leaders_with_paragraph = []
 
-            # Loop through the leaders of each country
+            # For each leader, get their Wikipedia first paragraph, add it to their dictionary, and append to the list
             for leader in leaders:
                 leader_url = leader["wikipedia_url"]
                 leader_first_paragraph = get_first_paragraph(session, leader_url)
@@ -107,25 +118,32 @@ def get_leaders():
                 leader_with_paragraph["first_paragraph"] = leader_first_paragraph
                 leaders_with_paragraph.append(leader_with_paragraph)
 
+            # Add the list of leaders to the dictionary under the current country
             leaders_per_country[country] = leaders_with_paragraph
 
-            # Display message to the user
+            # Print the progress for the user
             elapsed_time = time.time() - start_time
             print(f"Scraped leaders for {country}. Elapsed time: {elapsed_time:.2f} seconds.")
             time.sleep(1)  # Pause for 1 second to simulate processing time
-        
+
+    # Save the final dictionary to a file
     save(leaders_per_country)
 
     print("\nLeaders scraping completed.")
     print("Your JSON file is ready.")
 
+    # Return the final dictionary
     return leaders_per_country
+
 
 def get_first_paragraph(session, wikipedia_url):
     """
-    Retrieves and cleans the first paragraph from a given Wikipedia page
+    Function that retrieves the first meaningful paragraph from a Wikipedia page and performs a clean-up
+    operation to remove unwanted patterns.
     """
-    # Regular expressions to remove unwanted patterns from the paragraph
+
+    # List of regular expressions to be used for cleaning the paragraph.
+    # Each regex is aimed at removing a specific unwanted pattern.
     regex_list = [
         r"\ *\(/.+/\[e\].*\)", # Matches any text within parentheses with "/[e]" inside and optionally preceded by a space
         r"\ *\(/.+/.*\)", # Matches any text within parentheses optionally preceded by a space
@@ -138,39 +156,55 @@ def get_first_paragraph(session, wikipedia_url):
         r'\\\"'  # Matches occurrences of \"
     ]
 
+    # Request the content of the webpage
     response = session.get(wikipedia_url)
     html = response.text
+
+    # Parse the HTML content
     soup = BeautifulSoup(html, "html.parser")
 
-    # Parse through the HTML using BeautifulSoup and extract the first paragraph of the article
+    # Loop through all the paragraphs in the parsed HTML
+    # The goal is to find the first meaningful (non-empty) paragraph
     for paragraph in soup.find_all('p'):
-        if paragraph.text.strip():
+        if paragraph.text.strip():  # If the paragraph is not empty (has meaningful content)
             first_paragraph = paragraph.text.strip()
 
-            # Clean the first paragraph using the regular expressions defined above
+            # Clean the first paragraph by removing all unwanted patterns
+            # This is done by substituting each matching pattern with an empty string
             for regex in regex_list:
                 pattern = re.compile(regex)
                 first_paragraph = re.sub(pattern, "", first_paragraph)
 
+            # Once we have our first meaningful paragraph, exit the loop
             break
 
+    # Return the cleaned first paragraph
     return first_paragraph
 
-def check_scraping():
+
+def check_scraping(): 
+    """
+    Function that performs the whole scraping process, validates the scraped data with the saved data, 
+    and finally returns whether the scraping was successful.
+    """
+
+    # Display greeting message and explain the purpose of this script
     greet_user()
 
-    # Scrape the data again
+    # Scrape the leaders' data
     scraped_leaders = get_leaders()
 
-     # Read data from leaders.json
+    # Open 'leaders.json' file in read mode and load its data
     with open('leaders.json', 'r', encoding = "utf-8") as f:
         saved_leaders = json.load(f)
 
-    # Check if the variables match
+    # Check if the scraped data matches the saved data
     if saved_leaders == scraped_leaders:
-        print("\nSuccess! Scraping matched the saved data.")
+        # If they match, print a success message
+        print("\nSuccess! Scraping matched the saved data.\n")
     else:
+        # If they don't match, print an error message
         print("Error! Scraping didn't match the saved data. Try again.")
-    return saved_leaders == scraped_leaders
 
-result = check_scraping()
+    # Return True if the scraped data matches the saved data, else False
+    return saved_leaders == scraped_leaders
---
+++ main.py
@@ -0,0 +1,3 @@
+import leaders_scraper
+
+leaders = leaders_scraper.check_scraping()
