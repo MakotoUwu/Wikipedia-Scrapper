bac303da6ab94f90c393c7ebc684bc7f0b0d304b
Aleksandr <tsepuh.mamoru@gmail.com>
2023-06-23 06:39:34 +0200

    add structure to json file

--- leaders_scraper.py
+++ leaders_scraper.py
@@ -1,82 +1,122 @@
-# Import necessary libraries
-import requests
 import json
+import requests
 from bs4 import BeautifulSoup
 import re
 
-# Function to get leaders for each country
-def get_leaders():
-    # Define the urls
-    root_url = 'https://country-leaders.onrender.com'
-    status_url = root_url + '/status'
-    countries_url = root_url + '/countries'
-    leaders_url = root_url + '/leaders'
-    cookie_url = root_url + '/cookie'
+def save(leaders_per_country):
+    # Save leaders_per_country to leaders.json
+    with open('leaders.json', 'w') as f:
+        json.dump(leaders_per_country, f, indent=4, sort_keys=True)
 
-    # Get the cookies
-    cookies = requests.get(cookie_url).cookies
+def get_leaders():
+    # Define the URLs
+    root_url = "https://country-leaders.onrender.com"
+    countries_url = root_url + "/countries"
+    leaders_url = root_url + "/leaders"
+    cookie_url = root_url + "/cookie"
+
+    # Create a session
+    session = requests.Session()
+
+    # Get the initial cookies
+    cookies = session.get(cookie_url).cookies
 
     # Get the countries
-    req = requests.get(countries_url, cookies=cookies)
-    countries = req.json()
+    countries_response = session.get(countries_url, cookies=cookies)
 
-    # Initialize a dictionary to hold leader data
+    # Check if there is a cookie error
+    if countries_response.status_code == 401:
+        # Get new cookies
+        cookies = session.get(cookie_url).cookies
+        countries_response = session.get(countries_url, cookies=cookies)
+
+    # Check if there is an exception or another error
+    countries_response.raise_for_status()
+
+    countries = countries_response.json()
+
+    # Create an empty dictionary
     leaders_per_country = {}
 
-    # Initialize session
-    with requests.Session() as s:
-        # Loop over the countries and save their leaders in a dictionary
-        for country in countries:
-            try:
-                response = s.get(leaders_url + '?country=' + country, cookies=cookies)
-                response.raise_for_status()  # Check for HTTP errors
-                leaders = response.json()
-                
-                # Get Wikipedia info for each leader
-                for leader in leaders:
-                    if 'wikipedia' in leader:
-                        leader['first_paragraph'] = get_first_paragraph(leader['wikipedia'], s)
+    # Loop through the countries
+    for country in countries:
+        params = {"country": country}
+        leaders_response = None
 
-                leaders_per_country[country] = leaders
-            except requests.exceptions.HTTPError as err:
-                if err.response.status_code == 403:  # If cookie error
-                    cookies = requests.get(cookie_url).cookies
-                    response = s.get(leaders_url + '?country=' + country, cookies=cookies)
-                    leaders = response.json()
-                    
-                    # Get Wikipedia info for each leader
-                    for leader in leaders:
-                        if 'wikipedia' in leader:
-                            leader['first_paragraph'] = get_first_paragraph(leader['wikipedia'], s)
+        try:
+            leaders_response = session.get(leaders_url, cookies=cookies, params=params)
 
-                    leaders_per_country[country] = leaders
-                else:
-                    print(f"HTTP error occurred: {err}")
+            # Retry with new cookies if a "403 Forbidden" error occurs
+            while leaders_response.status_code == 403:
+                # Get new cookies
+                cookies = session.get(cookie_url).cookies
+                leaders_response = session.get(leaders_url, cookies=cookies, params=params)
+
+            # Check if there is an exception or another error
+            leaders_response.raise_for_status()
+
+        except requests.exceptions.HTTPError as e:
+            print("An error occurred:", e)
+            # Handle the error accordingly, e.g., retry or log the error.
+
+        if leaders_response is not None:
+            leaders = leaders_response.json()
+            leaders_with_paragraph = []
+
+            # Loop through the leaders of each country
+            for leader in leaders:
+                leader_url = leader["wikipedia_url"]
+                leader_first_paragraph = get_first_paragraph(session, leader_url)
+
+                leader_with_paragraph = leader.copy()
+                leader_with_paragraph["first_paragraph"] = leader_first_paragraph
+                leaders_with_paragraph.append(leader_with_paragraph)
+
+            leaders_per_country[country] = leaders_with_paragraph
+        
+    save(leaders_per_country)
+
     return leaders_per_country
 
+def get_first_paragraph(session, wikipedia_url):
+    # Code for retrieving the first paragraph from Wikipedia
 
-# Function to get first paragraph from Wikipedia
-def get_first_paragraph(wikipedia_url, session):
+    # Regular expressions to remove unwanted patterns from the paragraph
+    regex_list = [
+        r"\ *\(/.+/\[e\].*\)",  # Matches a pattern that starts with an optional space, followed by a forward slash, any character one or more times (except newline), followed by "/[e]", and then followed by any characters zero or more times.
+        r"\ *\(/.+/.*\)",  # Matches a pattern that starts with an optional space, followed by a forward slash, any character one or more times (except newline), and then followed by any characters zero or more times.
+        r"\[[0-9]+\]",  # Matches a pattern that starts with an opening square bracket, followed by one or more digits, and ends with a closing square bracket.
+        r"[\n]",  # Matches a newline character.
+        r"[\t]",  # Matches a tab character.
+        r"[\xa0]"  # Matches a non-breaking space character.
+    ]
+
     response = session.get(wikipedia_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    paragraphs = soup.find_all('p')
-    first_paragraph = ''
-    for para in paragraphs:
-        if para.text and len(para.text) > 100:
-            first_paragraph = re.sub(r'\[[^]]*\]', '', para.text)  # Remove references like [1]
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    
+    for paragraph in soup.find_all('p'):
+        if paragraph.text.strip():
+            first_paragraph = paragraph.text.strip()
+
+            for regex in regex_list:
+                pattern = re.compile(regex)
+                first_paragraph = re.sub(pattern, "", first_paragraph)
+                
             break
+    
     return first_paragraph
 
-# Function to save data into a json file
-def save(data, filename='leaders.json'):
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
 
-# Main function to get leaders and save data into a file
-def main():
-    leaders = get_leaders()
-    save(leaders)
 
-# Calling the main function
-if __name__ == "__main__":
-    main()
+# Call the function (1 line)
+result = get_leaders()
+print(result)
+
+# Read data from leaders.json
+with open('leaders.json', 'r') as f:
+    saved_leaders = json.load(f)
+
+# Check if the variables match
+print(saved_leaders == result)
+
